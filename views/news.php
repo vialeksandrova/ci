868bf25c6dc52ee0ed414d6d755b5b591f4bf1a3
vi aleksandrova <viialeksandrova@gmail.com>
2014-03-26 12:34:02 +0200

    Update news.php

--- views/news.php
+++ views/news.php
@@ -5,7 +5,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
 	<title>Form</title>
-	<!--<link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" charset="utf-8">-->
+	
 	<style type="text/css">
 	label {
 	display: block;
